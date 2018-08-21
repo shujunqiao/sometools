41e6cafb6b8d5431633e43117241d3bcb0db8d7e
乔书军 <qiaofeng0oo@126.com>
2018-08-21 18:51:36 +0800

    add file readlines.py.
    get lines of all files in folder.  need modify  file_path in readlines.py。

---
+++ src/readlines.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env pypy
+# -*- coding: utf-8
+
+import os
+
+# file_path = '/Users/qwe/workspace/proj/ma_shanxi3/src/hall/'
+file_path = '/Users/qwe/workspace/proj/ma_shanxi3/src/'
+
+def cleanArray(arr):
+	isnull = True
+	while isnull:
+		try:
+			arr.remove('')
+		except Exception, e:
+			isnull = False
+
+def getLinesOfFile(name):
+	p = os.popen('wc -l ' + name).read()
+	arr = p.split(' ')
+	cleanArray(arr)
+	return int(arr[0])
+
+def readFolders(path):
+	lines_sum = 0
+	for i in os.listdir(path):
+		f_p = os.path.join(path,i)
+		if os.path.isfile(f_p):
+			if i.find('DS_Store') == -1:
+				lines_sum = lines_sum + getLinesOfFile(f_p)
+	return lines_sum
+print readFolders(file_path)
+
+
+
