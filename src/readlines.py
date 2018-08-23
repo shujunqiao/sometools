7ace4243e1da3123dee081ba48e32e3aec02355f
乔书军 <qiaofeng0oo@126.com>
2018-08-23 16:30:21 +0800

    修改多层folder的读取。

--- src/readlines.py
+++ src/readlines.py
@@ -4,7 +4,8 @@
 import os
 
 # file_path = '/Users/qwe/workspace/proj/ma_shanxi3/src/hall/'
-file_path = '/Users/qwe/workspace/proj/ma_shanxi3/src/'
+# file_path = '/Users/qwe/workspace/proj/ma_shanxi3/src/'
+file_path = '/Users/qwe/workspace/proj/mahjong_common/src/'
 
 def cleanArray(arr):
 	isnull = True
@@ -24,9 +25,12 @@ def readFolders(path):
 	lines_sum = 0
 	for i in os.listdir(path):
 		f_p = os.path.join(path,i)
+		# print 'readFolders:', f_p
 		if os.path.isfile(f_p):
 			if i.find('DS_Store') == -1:
 				lines_sum = lines_sum + getLinesOfFile(f_p)
+		else:
+			lines_sum = lines_sum + readFolders(f_p)
 	return lines_sum
 print readFolders(file_path)
 
